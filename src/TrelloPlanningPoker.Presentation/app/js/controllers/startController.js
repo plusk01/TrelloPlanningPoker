33078765d1fb88bbfc4632a894c6889c635733d3
Byron Sommardahl <byron@acklenavenue.com>
2014-02-26 17:15:35 -0600

    Fixed start.

--- src/TrelloPlanningPoker.Presentation/app/js/controllers/startController.js
+++ src/TrelloPlanningPoker.Presentation/app/js/controllers/startController.js
@@ -3,38 +3,35 @@ angular.module('myApp.controllers')
     .controller('startController', ['$window', '$scope', '$routeParams', 'trelloService', 'gameService',
         function($window, $scope, $routeParams, trello, game) {
 
-            trello.onAuthError(function () {
+            trello.onAuthError(function() {
                 $window.location.href = "#/login";
             });
 
-            console.log($scope.list);
-            
-            if (!$scope.user) {
-                trello.getUser().then(function(u) {
+            trello.getUser().then(function(u) {
 
-                    $scope.user = u;
+                $scope.user = u;
 
-                    $scope.selectBoard = function(board) {
+                $scope.selectBoard = function(board) {
 
-                        trello.getLists(board.id).then(function(lists) {                            
-                            $scope.lists = lists;
-                        });
-                    };
-
-                    $scope.selectList = function (list) {
-                        $scope.tempName = $scope.board.name + '/' + list.name;
-                    };
-                    
-                    trello.getBoards(u.username).then(function(boards) {
-                        $scope.boards = boards;
-                    });
-                });
-
-                $scope.createGame = function() {
-                    if (!$scope.name) $scope.name = $scope.tempName;
-                    game.create($scope.user.username, $scope.board.id, $scope.list.id, $scope.name).then(function(gameInfo) {
-                        $window.location.href = "/#/game/" + gameInfo.data.id;
+                    trello.getLists(board.id).then(function(lists) {
+                        $scope.lists = lists;
                     });
                 };
-            }
+
+                $scope.selectList = function(list) {
+                    $scope.tempName = $scope.board.name + '/' + list.name;
+                };
+
+                trello.getBoards(u.username).then(function(boards) {
+                    $scope.boards = boards;
+                });
+            });
+
+            $scope.createGame = function() {
+                if (!$scope.name) $scope.name = $scope.tempName;
+                game.create($scope.user.username, $scope.board.id, $scope.list.id, $scope.name).then(function(gameInfo) {
+                    $window.location.href = "/#/game/" + gameInfo.data.id;
+                });
+            };
+
         }]);
